302019b76712c1d4b86a2b257adcd83b325936fb
danrey321 <dreyes9@ucmerced.edu>
2020-02-15 21:00:31 -0800

    Update script.js

--- js/script.js
+++ js/script.js
@@ -17,7 +17,17 @@ FSJS project 2 - List Filter and Pagination
    scoped to that function.
 ***/
 
+const showPage = (list, page) => {
+   /*
+   Loop over items in the list parameter
+   -- If the index of a list item is >= the index of the first
+   item that should be shown on the page
+   -- && the list item index is <= the index of the last item
+   that should be shown on the page, show it
+   */
 
+
+}
 
 
 /*** 
@@ -36,7 +46,20 @@ FSJS project 2 - List Filter and Pagination
 ***/
 
 
-
+const appendPageLinks = (list) => {
+   /*
+   1. Determine how many pages are needed for the list by dividing the
+   total number of list items by the max number of items per page
+   2. Create a div, give it the “pagination” class, and append it to the .page div
+   3. Add a ul to the “pagination” div to store the pagination links
+   4. for every page, add li and a tags with the page number text
+   5. Add an event listener to each a tag. When they are clicked
+   call the showPage function to display the appropriate page
+   6. Loop over pagination links to remove active class from all links
+   7. Add the active class to the link that was just clicked. You can identify that
+   clicked link using event.target
+   */
+   }
 
 /*** 
    Create the `appendPageLinks function` to generate, append, and add 
